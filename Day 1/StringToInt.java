4fa9f7ef2a51207fda47bc212771663eaa751626
anushasathia <anushasathia20@gmail.com>
2025-05-24 08:01:06 +0530

    Add files via upload

---
+++ Day 1/StringToInt.java
@@ -0,0 +1,8 @@
+public class StringToInt {
+    public static void main(String[] args) {
+        String numericString = "12345";
+        int number = Integer.parseInt(numericString);
+
+        System.out.println("Converted integer: " + number);
+    }
+}
