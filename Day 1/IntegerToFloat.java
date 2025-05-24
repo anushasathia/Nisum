36075ef2b3c1d74fb7f84973ed952d34a8bb90f3
anushasathia <anushasathia20@gmail.com>
2025-05-24 07:47:28 +0530

    Add files via upload

---
+++ Day 1/IntegerToFloat.java
@@ -0,0 +1,10 @@
+public class IntegerToFloat {
+    public static void main(String[] args) {
+        Integer number = 42;
+
+        Float floatValue = number.floatValue();
+
+        System.out.println("Integer value: " + number);
+        System.out.println("Converted to float: " + floatValue);
+    }
+}
